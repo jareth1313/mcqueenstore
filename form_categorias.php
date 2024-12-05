1b5929c2ed6e0ee84cb2791cc84a8c456aba83c2
Cesar <c3sarm01@gmail.com>
2024-12-04 23:29:36 -0700

    agregar categoria

--- form_categorias.php
+++ form_categorias.php
@@ -1,10 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insertar Categoría</title>
+   
+    <link rel="stylesheet" href="css/insertar_categoria.css">
+</head>
+<body>
+
 <?php 
   include('nav.php');
 ?>
 
-<form action="formularios/insertar_categ.php" method="POST">
-    <label>Nombre de la categoría:</label> <br>
-    <input type="text" name="nom_categoria" required> <br><br>
+<div class="container">
+    <form class="category-form" action="formularios/insertar_categ.php" method="POST">
+        <h2>Agregar Categoría</h2>
+        <label for="nom_categoria">Nombre de la categoría:</label>
+        <input class="input-field" type="text" id="nom_categoria" name="nom_categoria" required> <br>
 
-    <input type="submit" value="Guardar">
-</form>
+        <input class="submit-btn" type="submit" value="Guardar">
+    </form>
+</div>
+
+</body>
+</html>
